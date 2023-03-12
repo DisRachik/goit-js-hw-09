90ca309a71462b36041af0d8eeee5256318f5a3c
DisRachik <radchenkoig@gmail.com>
2023-03-12 22:08:41 +0200

    the first task is completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+const TIME_INTERVAL = 1000;
+let setId = null;
+
+const getRandomHexColor = function () {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+const getChangeColor = function () {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+const onStartMagic = e => {
+  getChangeColor();
+
+  setId = setInterval(() => {
+    getChangeColor();
+  }, TIME_INTERVAL);
+
+  e.currentTarget.setAttribute('disabled', '');
+};
+
+const onStopMagic = () => {
+  clearInterval(setId);
+  refs.startBtn.removeAttribute('disabled');
+};
+
+refs.startBtn.addEventListener('click', onStartMagic);
+refs.stopBtn.addEventListener('click', onStopMagic);
